7d2d0058651f581e76cada89443ea927611a6c78
ALUMILAI <76619481+1makam1@users.noreply.github.com>
2025-04-03 14:57:41 +0700

    Update placeidteleport.lua

--- placeidteleport.lua
+++ placeidteleport.lua
@@ -1,5 +1,5 @@
 local jobid = "a4b7053b-a7d3-46f1-948f-8db640a9ce9c"
-local tp = false
+local tp = true
 if game.JobId ~= jobid then
 	if tp then	
 		game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, jobid, game:GetService("Players").localPlayer)
