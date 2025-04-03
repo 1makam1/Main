587d6eb2796aec4aa11806643701351d1e2d19b4
ALUMILAI <76619481+1makam1@users.noreply.github.com>
2025-04-03 14:42:39 +0700

    placeidteleport.lua

--- placeidteleport.lua
+++ placeidteleport.lua
@@ -1,1 +1,6 @@
-local jid = "a4b7053b-a7d3-46f1-948f-8db640a9ce9c"
+local jobid = "a4b7053b-a7d3-46f1-948f-8db640a9ce9c"
+game:GetService("Lighting"):GetPropertyChangedSignal("TimeOfDay"):Connect(function()
+	if game.JobId ~= jobid then
+	    game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, jobid, game:GetService("Players").localPlayer)
+	end
+end)
