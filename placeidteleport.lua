ff14cf22a9852b29431ac46153ba344b7405836f
ALUMILAI <76619481+1makam1@users.noreply.github.com>
2025-04-03 14:53:48 +0700

    Update placeidteleport.lua

--- placeidteleport.lua
+++ placeidteleport.lua
@@ -1,9 +1,7 @@
 local jobid = "a4b7053b-a7d3-46f1-948f-8db640a9ce9c"
 local tp = false
-game:GetService("Lighting"):GetPropertyChangedSignal("TimeOfDay"):Connect(function()
-	if game.JobId ~= jobid then
-	    if tp then	
-	    	game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, jobid, game:GetService("Players").localPlayer)
-		end
+if game.JobId ~= jobid then
+	if tp then	
+		game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, jobid, game:GetService("Players").localPlayer)
 	end
-end)
+end
